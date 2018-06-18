ecf82a4b35f63fc9ae79caee3ea31e35c3daa71f
alisonBerchstollDeOliveira <alisondeoliveira@hotmail.es>
2018-06-18 08:33:44 -0400

    Version 0.5

--- js/script.js
+++ js/script.js
@@ -1,34 +1,70 @@
 var a = 0;
 var b = 0;
-var orden = ['Orden'];
+var prision_a=0;
+var prision_b=0;
+var x = Math.floor((Math.random() * 10) + 1);
+var ronda=1;
 
-//						Prisionero a 		Prisionero B
-//preguntas 			SI 	NO 				si 	no
-var preguntas = [
-'Conoces al Prisionero B',
-[4,6,4,5,4,6,4,4],
-'Usted conoce a Alison',
-[2,7,3,3,2,2,7,2]
-];
-console.log(preguntas);
-
-function selector(argument) {
-	if (argument=='SI') {
-		a++;
-		orden.push('SI');
-		console.log(orden);
+if (x>=6) {
+		$('#prisionero_b').text('Tu amigo decidio cooperar.');
 	}else{
-		b++;
-		orden.push("NO");
-		orden.push(orden);
-		console.log(orden);
+		$('#prisionero_b').text('Tu amigo no decidio cooperar.');
 	}
+function Cooperacion(prisionera_a,prisionero_b) {
+	if(prisionera_a=='SI' && prisionero_b=='SI'){
+		prision_a+=1;
+		prision_b+=1;
+		$('#prisionero_a').text('En la última ronda, decidiste cooperar.');
+	}else{
+		if(prisionera_a=='SI' && prisionero_b=='NO'){
+			prision_a+=2;
+			prision_b+=3;
+			$('#prisionero_a').text('En la última ronda, decidiste cooperar.');
+		}else{
+			if(prisionera_a=='NO' && prisionero_b=='NO'){
+				prision_a+=0;
+				prision_b+=0;
+				$('#prisionero_a').text('En la última ronda, decidiste no cooperar.');
+			}else{
+				if(prisionera_a=='NO' && prisionero_b=='SI'){
+					prision_a+=4;
+					prision_b+=2;
+					$('#prisionero_a').text('En la última ronda, decidiste no cooperar.');
+				}
+			}
+		}
+	}
+	x = Math.floor((Math.random() * 10) + 1);
+	if (x>=6) {
+		$('#prisionero_b').text('Tu amigo decidio cooperar.');
+	}else{
+		$('#prisionero_b').text('Tu amigo no decidio cooperar.');
+	}
+	console.log(prision_b);
+	console.log(prision_a);
+	ganador();
+	$('#ronda').text('Ronda '+ ++ronda);
+
 }
 
 $('#si').click(function(){
-	selector('SI');
+	if (x>=6) {
+		Cooperacion('SI','SI');	
+	}else{
+		Cooperacion('SI','NO');
+	}
 });
 
 $('#no').click(function(){
-	selector('NO');
+	if (x>=6) {
+		Cooperacion('NO','SI');	
+	}else{
+		Cooperacion('NO','NO');
+	}
 });
+
+function ganador(){
+	if (ronda==6) {
+		$('#principal').html('<div class="col s12 center" style="margin-top: 10%"><div class="row"><h4>Resultado</h4></div><div class="row"><h6 id="tu">Tu: '+prision_a+' años de prisión.</h6></div><div class="row"><h6 id="amigo">Amigo: '+prision_b+' años de prisión.</h6></div></div><div class="col s12" style="margin-top: 50px"><div class="row"><div class="col s6 center"><a id="inicio_session" class="waves-effect waves-light btn" onClick="history.go(0)">Nueva Partida</a></div><div class="col s6 center"><a id="inicio_session" class="waves-effect waves-light btn" href="../">Pagina Principal</a></div></div></div><script type="text/javascript"></script>');
+	}
+}
